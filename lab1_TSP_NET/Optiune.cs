13de5f4afa6208afc5c902e36995f116ab76a8af
Amalia D.S <damocsimonalina@gmail.com>
2020-03-25 10:46:26 +0200

    lab1

---
+++ lab1_TSP_NET/Optiune.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_TSP_NET
+{
+    public class Optiune
+    {
+        public delegate int Operation(int a, int b);
+        public event Operation operation;
+        public void Afisare(int a ,int b)
+        {
+            Console.WriteLine(operation(a,b));
+        }
+    }
+}
---
+++ lab1_TSP_NET/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_TSP_NET
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Optiune item = new Optiune();
+            item.operation += (a, b) => a + b;
+            item.Afisare(5, 3);
+            Console.WriteLine(PrimNumber(70));
+        }
+
+        public static int PrimNumber(int a)
+        {
+            for (int i = a-1; i > 0; i--)
+            {
+                int numarPrim = 0;              
+                for (int j=2; j <= i / 2; j++)
+                    {
+                    if (i % j == 0)
+                    {
+                            numarPrim ++;
+                    }
+                    if (numarPrim == 0)
+                    {
+                        return i;
+                    }
+                }  
+            }
+            return 0;
+        }
+    }
+}
